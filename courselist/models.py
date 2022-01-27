b34237f4b0a9319b5d33f4a9d2f4634bae22a894
BirhanuB <55416464+BirhanuB@users.noreply.github.com>
2022-01-26 20:58:27 -0500

    minor edit in models

--- courselist/models.py
+++ courselist/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Course(models.Model):
     courseTitle = models.CharField(max_length=100)
-    courseCode = models.CharField(max_length=6)
+    courseCode = models.CharField(max_length=8)
     creditHours = models.CharField(max_length=2)
